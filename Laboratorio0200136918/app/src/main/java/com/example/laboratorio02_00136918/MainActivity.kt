35d359f75d39a904edf8de5fac12147c3a823037
00218621 <00218621@uca.edu.sv>
2023-03-20 13:19:38 -0600

    Laboratorio02

---
+++ Laboratorio0200136918/app/src/main/java/com/example/laboratorio02_00136918/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.laboratorio02_00136918
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun BMIcalculate(view: View) {}
+}
